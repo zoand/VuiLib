c4b9268acc5269d730b26beafcd71ff41b4ea561
xcpp <xp2007@msn.com>
2023-04-30 14:12:53 +0800

    sync

--- demo/vml-viewer/vml-viewer/main.cpp
+++ demo/vml-viewer/vml-viewer/main.cpp
@@ -5,7 +5,7 @@
 
 #pragma comment(lib, "comsuppw.lib")
 
-int main(int args, const char *argvs[])
+int main(int args, const char* argvs[])
 {
 	if (args >= 2)
 	{
@@ -14,14 +14,14 @@ int main(int args, const char *argvs[])
 
 		_bstr_t RawString = argvs[1];
 
-		std::wstring FilePath((const wchar_t *)RawString);
+		std::wstring FilePath((const wchar_t*)RawString);
 
 		int			 SliptPosition = static_cast<int>(FilePath.find_last_of(L"\\")) + 1;
-		std::wstring Workspace	   = FilePath.substr(0, SliptPosition);
+		std::wstring Workspace = FilePath.substr(0, SliptPosition);
 
 		_wchdir(Workspace.c_str());
 
-		auto Result = MainWindow.LoadVML((const wchar_t *)RawString, VML::VMLParserParseMode::FromFile);
+		auto Result = MainWindow.LoadVML((const wchar_t*)RawString, VML::VMLParserParseMode::FromFile);
 
 		return App.Exec();
 	}
